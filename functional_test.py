88022eb06b5a25529b789898e67b5604b59decd3
Manu Grandío <manugrandio@fastmail.fm>
2019-09-08 00:11:36 +0200

    Add selenium test

--- functional_test.py
+++ functional_test.py
@@ -1,10 +1,24 @@
 from unittest import TestCase
 
 from requests import get
+from selenium import webdriver
 
 
 class TestDjangoServer(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.browser = webdriver.Firefox()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.browser.close()
+
     def test_server_is_running(self):
         body = get('http://localhost:8000').content
 
         self.assertIn(b'Django', body)
+
+    def test_server_is_running_selenium(self):
+        self.browser.get('http://localhost:8000/records/')
+
+        self.assertIn('Records', self.browser.title)
--- leanledger/records/tests/test_views.py
+++ leanledger/records/tests/test_views.py
@@ -16,6 +16,7 @@ class TestRecordsViews(TestCase):
     def test_records_page(self):
         url = reverse('records_list')
         content = self.client.get(url).content
+
         soup = BeautifulSoup(content, 'html.parser')
         rows = soup.find('table').find('tbody').find_all('tr')
         total = sum(int(row.find_all('td')[-1].string) for row in rows)
