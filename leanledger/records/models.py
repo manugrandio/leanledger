6e26673461d4c9cbfca97bb14c7c345c95e0bb11
Manu Grandío <manugrandio@fastmail.fm>
2019-09-09 07:35:18 +0200

    `Ledger` model

--- leanledger/records/models.py
+++ leanledger/records/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Ledger(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=64)
+
+
 class Account(models.Model):
     ORIGIN = 'O'
     DESTINATION = 'D'
@@ -13,3 +18,12 @@ class Account(models.Model):
     name = models.CharField(max_length=64)
     parent = models.ForeignKey(
         'self', null=True, on_delete=models.CASCADE, related_name='children')
+
+
+# class Record(models.Model):
+    # ledger = models.ForeignKey(Ledger)
+
+
+# class Variation(models.Model):
+    # record = models.ForeignKey(Record)
+    # account = models.ForeignKey(Account)
--- leanledger/records/tests/test_models.py
+++ leanledger/records/tests/test_models.py
@@ -1,19 +1,31 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-from leanledger.records.models import Account
+from leanledger.records.models import Account, Ledger
+
+
+class TestLedger(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        user = User.objects.create_user('Test')
+        cls.ledger = Ledger.objects.create(user=user, name='My Ledger')
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.ledger.delete()
+
+    def test_ledger(self):
+        ledger = Ledger.objects.all().get()
+
+        self.assertEqual(ledger.name, 'My Ledger')
 
 
 class TestAccount(TestCase):
     @classmethod
     def setUpClass(cls):
-        cash = Account(name='cash', type=Account.DESTINATION)
-        cash.save()
-
-        bank_one = Account(name='bank one', type=Account.DESTINATION, parent=cash)
-        bank_one.save()
-
-        bank_two = Account(name='bank two', type=Account.ORIGIN, parent=cash)
-        bank_two.save()
+        cash = Account.objects.create(name='cash', type=Account.DESTINATION)
+        bank_one = Account.objects.create(name='bank one', type=Account.DESTINATION, parent=cash)
+        bank_two = Account.objects.create(name='bank two', type=Account.ORIGIN, parent=cash)
 
     @classmethod
     def tearDownClass(cls):
