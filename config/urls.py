80726ae767305dccb283496a553c26492eb5fc86
Manu Grandío <manugrandio@fastmail.fm>
2019-09-30 21:22:13 +0200

    Root redirects to `ledger/`

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
+from django.shortcuts import redirect
 from django.urls import include, path
 
 
 urlpatterns = [
+    path('', lambda r: redirect('ledgers'), name='root'),
     path('ledger/', include('leanledger.ledger.urls')),
 ]
