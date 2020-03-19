d5530e07c159fa60da94e12ab5e1f9f92a86debd
Manu Grandío <manugrandio@fastmail.fm>
2020-03-19 11:33:28 +0100

    Record detail view loads data from backend

--- static/js/Record.js
+++ static/js/Record.js
@@ -6,41 +6,45 @@ const CREDIT = "credit";
 
 
 class Record extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {record: null};
+    this.getRecord();
+  }
+
+  getRecord() {
+    const self = this;
+    const recordUrl = this.getRecordUrl();
+    return fetch(recordUrl)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        self.setState({record: data});
+      });
+  }
+
+  getRecordUrl() {
+    // TODO clean
+    const currentUrl = new URL(window.location.href);
+    const pathUrlParts = currentUrl.pathname.split("/");
+    const ledgerId = pathUrlParts[2];
+    const recordId = pathUrlParts[pathUrlParts.length - 2];
+    const urlPath = `/ledger/${ledgerId}/record/${recordId}.json`;
+    return currentUrl.origin + urlPath;
+  }
+
   render() {
-    const record = {
-      is_balanced: true,
-      date: "2019-12-31",
-      variations: {
-        debit: [
-          {
-            id: 1,
-            accountName: "cash",
-            accountURL: "http://localhost:8000/ledger/1/account/2/",
-            amount: 100
-          },
-        ],
-        credit: [
-          {
-            id: 2,
-            accountName: "expense one",
-            accountURL: "http://localhost:8000/ledger/1/account/3/",
-            amount: 50
-          },
-          {
-            id: 3,
-            accountName: "expense two",
-            accountURL: "http://localhost:8000/ledger/1/account/4/",
-            amount: 50
-          },
-        ],
-      },
-    };
-    return(
-      <div className="card mt-3 shadow-sm">
-        <RecordHeader {...record}/>
-        <RecordBody {...record}/>
-      </div>
-    );
+    if (this.state.record === null) {
+      return <p className="mt-3">Loading...</p>;
+    } else {
+      return (
+        <div className="card mt-3 shadow-sm">
+          <RecordHeader {...this.state.record}/>
+          <RecordBody {...this.state.record}/>
+        </div>
+      );
+    }
   }
 }
 
@@ -106,11 +110,11 @@ class Variation extends Component {
       <tr>
         <td scope="row">
           <span className={ this.props.variationType === CREDIT ? 'ml-5' : '' }>
-            <a href={ this.props.accountURL }>{ this.props.accountName }</a>
+            <a href={ this.props.account_url }>{ this.props.account_name }</a>
           </span>
         </td>
-        { this.props.variationType === DEBIT ? valueColumn : emptyColumn }
-        { this.props.variationType === DEBIT ? emptyColumn : valueColumn }
+        { this.props.type === DEBIT ? valueColumn : emptyColumn }
+        { this.props.type === DEBIT ? emptyColumn : valueColumn }
       </tr>
     );
   }
